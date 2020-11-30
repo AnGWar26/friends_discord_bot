6b1ce2ed15b3be918be17e094c4de78ba2af24a4
AnGWar26 <andrew.warfield@protonmail.com>
2020-11-30 15:45:14 -0800

    move from client to bot - create mock function that takes previous message and randomly capitalizes it

--- bot.py
+++ bot.py
@@ -1,15 +1,54 @@
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+# this function randomly capitalizes letters in a sentence
+def capi_sentence(sentence):
+    new_sentence = ""
+    number = 0 #Dummy number for tracking
+
+    for letter in sentence.lower():
+        if len(new_sentence)<2: #Creates the first two letter
+            random_number = random.randint(0,1) #This randomly decides if the letter should be upper or lowercase
+            if random_number==0:
+                new_sentence += letter.upper()
+            else:
+                new_sentence += letter
+        else:
+            if (new_sentence[number-2].isupper() and new_sentence[number-1].isupper() or new_sentence[number-2].islower() and new_sentence[number-1].islower())==True:
+                #Checks if the two letters before are both upper or lowercase
+                if new_sentence[number-1].isupper(): #Makes the next letter the opposite of the letter before
+                    new_sentence += letter.lower()
+                else:
+                    new_sentence += letter.upper()
+            else:
+                random_number = random.randint(0,1)
+                if random_number==0:
+                    new_sentence += letter.upper()
+                else:
+                    new_sentence += letter
+                
+        number += 1 #Add one more to the tracking
+     
+    return(new_sentence)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-client.run(TOKEN)
+@bot.command(name='mock')
+async def mock_func(ctx):
+    channel = bot.get_channel(783045227321098301)
+    msg_history = await channel.history(limit=10).flatten() # create list of message objects 
+    previous_message = msg_history[1].content # get the message beofore $mock command
+    await ctx.send(capi_sentence(previous_message))
+
+bot.run(TOKEN)
